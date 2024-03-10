3d98992df1bbab05364b2b998bf3061e063cce62
Dildar Razza Laskar <dildarrazzalaskar@Dildars-MacBook-Air.local>
2024-03-11 01:17:28 +0530

    episode -01

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent=React.createElement("div",{id:"parent"},
+[React.createElement("div",{id:"child1"},
+[React.createElement("h1",{},"Hi i am a h1 tag"),
+React.createElement("h2",{},"Hi i am a h2 tag")]),React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"Hi i am a h1 tag"),
+React.createElement("h2",{},"Hi i am a h2 tag")])]);
+
+console.log(parent);
+
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+    
+    root.render(parent); 
